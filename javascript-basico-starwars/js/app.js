285b9aebbe1dd7eb6d70c49397337231e5f75a95
Simone Dos Santos Silva <wirus1@gmail.com>
2023-05-09 15:25:45 -0300

    Javascript
    Atualizando arquivos

---
+++ javascript-basico-starwars/js/app.js
@@ -0,0 +1,6 @@
+const App = new Vue({ // a constante App é uma nova instância do framework Vuejs
+    el: '#app', // id que está na div do html
+    data: {
+        title: 'Star Wars Lego'
+    }
+})
